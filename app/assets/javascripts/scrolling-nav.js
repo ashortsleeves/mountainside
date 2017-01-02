4bfece9cbc76d098001f1b823cced19923701f1c
ashortsleeves <ashortsleeves@gmail.com>
2017-01-02 16:12:36 -0500

    0.1.0 import site to rails

---
+++ app/assets/javascripts/scrolling-nav.js
@@ -0,0 +1,41 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#789f6e');
+          $(".navbar-brand").css('opacity', '1.0');  
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+          $(".navbar-brand").css('opacity', '0.0');  
+       }
+   });
+    }
+});
+
+ 
